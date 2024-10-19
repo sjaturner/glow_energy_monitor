726c0b308ace706cdf6b5b2adeb7e2f15972a835
simont <sjaturner@googlemail.com>
2024-10-19 18:23:06 +0100

    more tweaks, work around weird zero times

--- collect.sh
+++ collect.sh
@@ -5,7 +5,7 @@ while true ; do
         touch $energy
         ./recent.sh $energy.consumption > $energy.$(date +%s) 
         cat $energy $energy.* | sort -n | awk '{this = $1; if (last != this) {printf("%d\t%f\n", last, maxi);last=this;maxi=0.0;} else if ($2 > maxi) {maxi = $2;}; last = $1;}' | sponge $energy
-        octave plot.m $energy
+        octave -q plot.m $energy 2> /dev/null
     done
     sleep $(( 30 * 60 ))
 done
